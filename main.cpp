1ecdcb646e79d8c260222fa4859801c9c0d404b5
salmakl <salma.kalkhi@gmail.com>
2021-01-21 14:30:34 +0100

    file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include<stdio.h>
-int add(int a,int b){
+void add(int t[3],int t2[3]){
+	  int result[4],int rest[4]=0;
+    for(int i<4;i=0;i--){
+        if(bin1[i]==1&&bin2[i]==1)
+        rest[i]=0;
+        rest[i+1]=1;
+        else
+        rest[i]=0;
+    }
+    for(int i=0;i<4;i++){
+        if(bin1[i]==1&&bin2[i]==0&&rest[i+1]==0||bin1[i]==0&&bin2[i]==1&&rest[i]==0)
+        result[i]=1;
+        else if(bin1[i]==0&&bin2[i]==0)
+        result[i]=1;
+    }
+    for(int i=0;i<4;i++){
+        printf("%d ",result[i]);
 	
-	return 0;
 }
 int ban2int (){
 
