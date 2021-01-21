2bb0401e01b74bf6e77bdbab11ca1186f67afd03
Ayoub zoubiri <ayoub zoubiri>
2021-01-21 14:29:54 +0100

    AYOUB commit 2

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include<stdio.h>
 int add(int a,int b){
-	
+
 	return 0;
 }
 int ban2int (){
 
+    int  num, binary_val, decimal_val = 0, base = 1, rem;
+
+    printf("Enter a binary number(1s and 0s) \n");
+    scanf("%d", &num); /* maximum five digits */
+    binary_val = num;
+    while (num > 0)
+    {
+        rem = num % 10;
+        decimal_val = decimal_val + rem * base;
+        num = num / 10 ;
+        base = base * 2;
+    }
+    printf("The Binary number is = %d \n", binary_val);
+    printf("Its decimal equivalent is = %d \n", decimal_val);
 
 return 0;
 
@@ -17,5 +31,7 @@ int integertobinary (){
 }
 
 int main(int argc, char** argv) {
+
+    ban2int() ;
 	return 0;
 }
