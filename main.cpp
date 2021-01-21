a585c90b728198ff2e402a001a831e6c3e3a343f
MohamedBoumlik <mohamed.boumlik.19@gmail.com>
2021-01-21 15:57:04 +0100

    v2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include<stdio.h>
 
-int integer to binary (){
+int *integertobinary (int a){
 
-    return 0;
+    int i = 0,j=0,b[4],bin;
+    while(a !=0){
+        b[j]=a%2;
+        bin=a*i;
+        a=a/2;
+        i=i*10;
+        j++;}
+	return 0;
+   
 }
 
 int main(int argc, char** argv) {
